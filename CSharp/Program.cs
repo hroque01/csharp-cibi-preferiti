70eec6fe3fc130f2aff15f6e363bdefdc297382c
Hadson Roque <nineonezero.light@gmail.com>
2023-04-04 14:28:08 +0200

    Print my favourite food

--- CSharp/Program.cs
+++ CSharp/Program.cs
@@ -4,10 +4,14 @@
 string[] FoodsArray = { "Pasta alla Carbonara", "Pizza Diavola", "Nighiri al salmone", "Piadina Kebab", "Torta al cioccolato", "Risotto Mantovano", "Adobo", "Tofu fritto", "Ravioli gamberi" };
 
 //Print Lenght
-Console.WriteLine(FoodsArray.Length);
+Console.WriteLine("Your list is made with " + FoodsArray.Length + " element");
 
 //Print array
 for (int i = 0; i < FoodsArray.Length; i++)
 {
-    Console.WriteLine(i + 1 + "" + FoodsArray[i]);
+    Console.WriteLine(i + 1 + " " + FoodsArray[i]);
 }
+
+//Print my favourite dishes
+var myFavourite = FoodsArray.First();
+Console.WriteLine("My favourite is " + myFavourite);
